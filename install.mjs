4e8cc5693dc12b0ce91b9bbc526efa350dfbc80c
Edvard Chen <pigeon73101@gmail.com>
2022-03-11 22:42:41 +0800

    fix(trojan): fix wrong config path

--- install.mjs
+++ install.mjs
@@ -1,23 +1,22 @@
 const { fs } = require("zx");
 
-const [, , , sshServer = "10.0.2.1", configFolder = "/jffs/jianhua/trojan", port = "1081"] =
-  process.argv;
+const [, , , sshServer = "10.0.2.1", configFolder = "/jffs/jianhua/", port = "1081"] = process.argv;
 
 // unzip trojan
 const trojanZip = "trojan-go-linux-arm.zip";
-await $`unzip ./trojan/${trojanZip} ${path.basename(trojanZip)}`;
+await $`unzip ./trojan/${trojanZip} -d ./trojan/trojan-go-linux-arm`;
 
 // replace config_home with user defined folder
 for (const file of ["./trojan/apply_iptables", "./trojan/dnsmasq.conf.add"]) {
   await replaceText(file, (content) => {
-    return content.replace("$config_home", configFolder).replace("$port", port);
+    return content.replace(/\$config_home/g, configFolder).replace(/\$port/g, port);
   });
 }
 
 // upload essential resources
 await $`rsync -av ./trojan ${sshServer}:${configFolder}`;
 
-const command = `sh ${configFolder}/setup.sh`;
+const command = `sh ${configFolder}/trojan/setup.sh`;
 await $`ssh ${sshServer} ${command}`;
 
 async function replaceText(file, fn) {
